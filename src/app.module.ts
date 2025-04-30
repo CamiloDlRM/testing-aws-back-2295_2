314cf9229b732e2dff79109ced468d6b56d150c7
J-Povea21 <jpoveafernandez21@gmail.com>
2025-04-29 23:50:57 -0500

    Made PrismaService accesible globally

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
