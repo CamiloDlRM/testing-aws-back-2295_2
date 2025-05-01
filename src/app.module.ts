853b3d76c57b34a94aca3e2225a55a41e7c1c430
J-Povea21 <jpoveafernandez21@gmail.com>
2025-05-01 18:25:52 -0500

    Added authentication module and new README.md with instructions

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,8 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { SupabaseAuthGuard } from './auth/guards/supabase-auth.guard';
-import { UsersModule } from './users/users.module';
-
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule, UsersModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule],
   controllers: [AppController],
   // We provide the AuthGuard as a global guard for all routes
   providers: [AppService, { provide: APP_GUARD, useClass: SupabaseAuthGuard }],
